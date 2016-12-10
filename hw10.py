c5ad7d523c4ee06a155dd3eeda0e0a281c85d639
Kyle Su <sukyle@seas.upenn.edu>
2016-12-10 16:47:49 -0500

    initial start

---
+++ hw10.py
@@ -0,0 +1,173 @@
+''' Homework 10
+-- Due Tuesday, November 22 at 11:59pm
+-- Before starting, read
+https://www.cis.upenn.edu/~cis192/homework/
+-- Always write the final code yourself
+-- Cite any websites you referenced
+-- Use the PEP-8 checker for full style points:
+https://pypi.python.org/pypi/pep8
+'''
+
+# In this HW, you'll create an API for serving basic information about Penn
+# courses. Note that the provided csv file is a few years old...
+#
+# First, you'll convert a CSV file containing Registrar data to JSON
+# format.  Next, you'll use this data as a source for serving through
+# your API.  Helper functions will probably be useful!
+
+import csv
+import json
+import os
+from flask import Flask, request, jsonify
+
+
+def csv_to_json(csvfile, jsonfile):
+    ''' Load the data in csvfile, convert it to a list of dictionaries,
+    and then save the result as JSON to jsonfile.
+
+    You can assume that the first line of csvfile will include the field names.
+    Each following line should be split into its fields and turned into a
+    dictionary. The output should be a list of dictionaries.
+
+    Hint: csv.DictReader will help here!
+    '''
+
+    dict_list = []
+    with open(csvfile) as data_file:
+        reader = csv.DictReader(data_file)
+        for row in reader:
+            dict_list.append(row)
+    with open(jsonfile, 'w+') as output_file:
+        json.dump(dict_list, output_file)
+
+
+def load_json(jsonfile):
+    ''' Load JSON data from the given filename. Note that this should
+    return Python data structures, not a string of JSON.
+
+    If jsonfile does not exist, raise an exception with the message
+    "No such file." If jsonfile does not contain valid JSON, raise an
+    exception with the message "Invalid JSON."
+    '''
+    if not os.path.isfile(jsonfile):
+        raise NameError("No such file.")
+    with open(jsonfile) as data_file:
+        try:
+            data = json.load(data_file)
+        except ValueError as e:
+            raise ValueError("Invalid JSON.")
+
+    return data
+
+
+# Convert courses.csv to courses.json (uncomment when csv_to_json is
+# implemented).
+csv_to_json('courses.csv', 'courses.json')
+
+# Load the course data into a global variable COURSES, which should be used by
+# your Flask application (uncomment when load_json is implemented).
+COURSES = load_json('courses.json')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    '''This is what you will see if you go to http://127.0.0.1:5000.
+    You don't need to modify this.'''
+    return 'Hello world!'
+
+
+@app.route('/courses/<dept>')
+@app.route('/courses/<dept>/<code>')
+@app.route('/courses/<dept>/<code>/<section>')
+def courses(dept, code=None, section=None):
+    ''' Returns a list of courses matching the query parameters.
+
+    The response should be JSON of the following format:
+    { "results": [list of courses] }
+    Each course in the list should be represented by a dictionary.
+
+    ** Note that JSON should never have lists at the top level due to security
+    issues with JavaScript! **
+    See http://flask.pocoo.org/docs/security/#json-security.
+    That's why we return a dictionary with one key/value pair instead.
+
+    For any parameters not provided, match any value for that parameter.
+    For instance, accessing /courses/cis should return a list of all CIS
+    courses, and accessing courcies/cis/110 should return a list of all
+    sections for CIS 110.
+
+    There is also an optional GET request parameter for the 'type' key
+    found in the CSV file. You should detect that and use it if it is provided.
+    For instance, accessing /courses/cis?type=REC should return a list of all
+    CIS recitation sections.
+    '''
+
+    return_list = []
+    if code is None and section is None:
+        if 'type' in request.args:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['type'] == request.args.get('type')]
+        else:
+            return_list = [item for item in COURSES if item['dept'] == dept]
+    elif code is None:
+        if 'type' in request.args:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['section'] == section and item['type'] == request.args.get('type')]
+        else:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['section'] == section]
+    elif section is None:
+        if 'type' in request.args:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['code'] == code and item['type'] == request.args.get('type')]
+        else:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['code'] == code]
+    else:
+        if 'type' in request.args:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['code'] == code and item['section'] == section and
+                           item['type'] == request.args.get('type')]
+        else:
+            return_list = [item for item in COURSES if item['dept'] == dept and
+                           item['code'] == code and item['section'] == section]
+    data = {}
+    data["results"] = return_list
+    json_data = jsonify(data)
+
+    return json_data
+
+
+# Implementing schedule() below is +10 extra credit!
+
+@app.route('/schedule', methods=['POST'])
+def schedule():
+    ''' A POST-only endpoint. Accept POST data of the form:
+    {'courses': [ list of courses ]}
+
+    Each course dictionary in the list should have 'dept', 'code', and
+    'section' keys. Look up each course and find its meeting times.
+    (If the given dictionary does not match any course, just skip it.)
+    Construct and return a corresponding JSON schedule, where keys are the
+    days of the week, and values are a list of courses that meet on that day.
+    See the test case for details. You can assume that the provided courses
+    don't conflict.
+
+    Some notes:
+    1. Remember that dictionaries are unordered data structures, so the order
+    of keys does not matter.
+    2. The class lists should be in alphabetical order of class name.
+    3. If there is no class on a day, don't include a key for that day.
+    '''
+    pass
+
+
+def main():
+    app.debug = True
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
