fe359076b0eaa7a49e1ef05de3b4dbc2215edc55
Kyle Su <sukyle@seas.upenn.edu>
2016-12-13 01:04:55 -0500

    final project

--- README.md
+++ README.md
@@ -1,20 +1,37 @@
-# CIS192Project
-In Twitter Bot, we parse through the HTMl of the website using BeautifulSoup,
-www.twittercounter.com, in order to access the Twitter handle of
-the top 100 users.
+# CIS192Project, Claire Huang & Kyle Su
+The purpose of Twitter Bot is to take the top 100 most followed accounts on Twitter
+and collect their most recent tweets to track hashtag usage. Our app displays for each user
+all hashtags used, as well as number of times used. For individual hashtags, our app displays
+all users that used that hashtag in addition to the number of uses per user, and total.
 
-To store the data needed, there is a "users" dictionary and "hashtags" dictionary.
-Each username will be stored as a key in the "users" dictionary,
-and the value will be a dictionary of keys of hashtags and  values of the
-count of each hashtag. 
+Our homepage has two search bars. You can either search by Twitter user name, or by specific
+hashtags. Capitalization matters. If you searched for a valid user, you'll see a list of all
+hashtags that user has used in their past XX tweets (modifiable upto past 200), as well as
+the number of times each hashtag was used. There will also be a link to that Twitter account's
+page. If you enter a valid hashtag, you will see a list of all users who have used the tag,
+as well as the total number of times the tag was used. Our homepage also displays the sum total
+of all hashtags used, as well as the ten most popular hashtags by number of mentions.
 
-In the "hashtags" dictionary, we store the hashtag as a key and the value as
-an instance of our user created class, Hashtag, which stores information about
-the text, the user and times the hashtag is used. This information is found using
-the Twitter REST API to access the top 100 handles' tweets' qualities.
+Modules used:
+    json - Used to return False data in AJAX calls
+    functools - Used to override rich comparision magic methods
+    operator - Used to sort items
+    flask - Used to route URLs, pass data back and forth from frontend to backend
+    requests - Used to access information passed from frontend
+    bs4 - Used BeautifulSoup to crawl websites to find Twitter usernames
+    tweepy - Used to ease usage of Twitter API
 
-We use the generator to iterate through the list of Hashtag objects to count the
-number of hashtags.
+Custom class requirement:
+    We implemented a Hashtag class to store information about individual hashtags. The "text"
+field stores the actual string of the hashtag. The "count" field tracks the total number of times
+the hashtag was used by all users. The "users" field is a dictionary with keys="username",
+value="# times this hashtag was used by that username".
+    We overrode the rich comparision magic methods (unused), as well as the __len__ method. We
+used our __len__ method to return the "count" of the object, which is used to accumulate the
+total number of times all hashtags are used (displayed on our homepage).
 
-We overrode the __len__ magic method in order to get the number of times a
-hashtag was used.
+Generator function requirement:
+    In pull_info() we used a generator function to accumulate the total number of hashtags used.
+Instead of creating another list of all the individual Hashtag objects (which are containe in a
+dictionary), we used the generator to iterate through the objects one by one.
+
--- project.py
+++ project.py
@@ -1,19 +1,12 @@
-''' Homework 10
--- Due Tuesday, November 22 at 11:59pm
--- Before starting, read
-https://www.cis.upenn.edu/~cis192/homework/
--- Always write the final code yourself
--- Cite any websites you referenced
--- Use the PEP-8 checker for full style points:
-https://pypi.python.org/pypi/pep8
-'''
-
-# In this HW, you'll create an API for serving basic information about Penn
-# courses. Note that the provided csv file is a few years old...
+# In this project, we used the Twitter API to accumulate tweets from
+# the top 100 most followed users on Twitter. These tweets were used
+# to form a snapshot of recent hashtag usage
 #
-# First, you'll convert a CSV file containing Registrar data to JSON
-# format.  Next, you'll use this data as a source for serving through
-# your API.  Helper functions will probably be useful!
+# First, we used BeautifulSoup to crawl through a webpage to find the
+# usernames of the top 100 most popular Twitter accounts. Secondly,
+# we used the Twitter REST APIs to grab the 50 (adjustable) most recent
+# tweets/retweets by that user. These tweets are parsed for their hashtags,
+# information about which is displayed on a webpage
 
 
 import json
@@ -70,11 +63,18 @@ total_hashtags = 0
 
 
 def get_tweets(username):
+    ''' Given a Twitter username, the Twitter REST API was used to
+    grab the most recent tweets/retweets by that user. The hashtags
+    used for each tweet are found and used to update our list of
+    users and individual hashtags '''
+
+    ''' Create a new user in users '''
     if username not in users:
         users[username] = {}
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
+    ''' Grab tweets '''
     new_tweets = api.user_timeline(screen_name=username, count=50)
     for status in new_tweets:
         list_text = status.entities.get('hashtags')
@@ -94,61 +94,78 @@ def get_tweets(username):
                 hashtags[hashtag_text] = new_hashtag
                 hashtags[hashtag_text].addTweet(username)
 
+
 def pull_info():
+    ''' Grab usernames of top 100 Twitter users and call get_tweets() for each
+    user. Once all tweets are parsed, calculate the total number of hashtags
+    used, as well as the top ten hashtags by usage '''
+
     TWEETS = 'http://twittercounter.com/pages/100'
     r = requests.get(TWEETS)
     soup = BeautifulSoup(r.text, "lxml")
 
-    l = list()
     full_tag = soup.findAll('span', {"itemprop": True})
-
     for tag in full_tag:
         if "alternateName" in tag['itemprop']:
             get_tweets(tag.text[1:])
-            l.append(tag.text[1:])
 
     r.connection.close()
 
-    hashtag_counter = (len(hashtag_object) for hashtag_object in hashtags.values())
+    ''' Use generator to accumulate total number of tweets'''
+    hashtag_counter = (len(hashtag_object) for hashtag_object in
+                       hashtags.values())
     global total_hashtags
     for x in range(0, len(hashtags.keys())):
         total_hashtags += next(hashtag_counter)
-    topten_hashtags = sorted(hashtags.values(), key=operator.attrgetter('count'), reverse=True)[:10]
+    topten_hashtags = sorted(hashtags.values(),
+                             key=operator.attrgetter('count'),
+                             reverse=True)[:10]
 
+    ''' Get top ten tweets and store data as tuple to pass to index.html '''
     for i in range(len(topten_hashtags)):
         hashtag_object = topten_hashtags[i]
-        hashtag_data = (getattr(hashtag_object, 'text'), getattr(hashtag_object, 'count'))
+        hashtag_data = (getattr(hashtag_object, 'text'),
+                        getattr(hashtag_object, 'count'))
         topten_data.append(hashtag_data)
+
 
 @app.route('/')
 def home():
     '''This is what you will see if you go to http://127.0.0.1:5000.'''
-    global total_hashtags
-
-    return render_template('index.html', num_hashtags=total_hashtags, topten_data=topten_data)
+    global total_hashtag
+    return render_template('index.html', num_hashtags=total_hashtags,
+                           topten_data=topten_data)
 
 
 @app.route('/searchuser', methods=['POST'])
 def searchUser():
+    ''' Search for provided username, redirect if found and pass used hashtags
+    sorted by usage '''
     if request.form.get('name') in users:
         user = request.form.get('name')
-        sorted_hashtags = sorted(users[user].items(), key=operator.itemgetter(1), reverse=True)
-        return render_template('user.html', username=user, hashtags=sorted_hashtags)
+        sorted_hashtags = sorted(users[user].items(),
+                                 key=operator.itemgetter(1), reverse=True)
+        return render_template('user.html', username=user,
+                               hashtags=sorted_hashtags)
     else:
         return json.dumps(False)
 
 
 @app.route('/searchhashtag', methods=['POST'])
 def searchHashtag():
+    ''' Search for provided hashtag, redirect if found and pass all users
+    who have used said hashtag soeted by usage '''
     if request.form.get('hashtag') in hashtags:
         tag = request.form.get('hashtag')
 
         hashtag_object = hashtags[tag]
         num_uses = getattr(hashtag_object, 'count')
         unsorted_users = getattr(hashtag_object, 'users')
-        sorted_users = sorted(unsorted_users.items(), key=operator.itemgetter(1), reverse=True)
+        sorted_users = sorted(unsorted_users.items(),
+                              key=operator.itemgetter(1), reverse=True)
 
-        return render_template('hashtag.html', tag=tag, num_uses=num_uses, users=sorted_users)
+        return render_template('hashtag.html', tag=tag, num_uses=num_uses,
+                               users=sorted_users)
     else:
         return json.dumps(False)
 
